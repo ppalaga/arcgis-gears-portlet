8ca70eebabb9804e2ca873020be9f5069b6669a4
Peter Palaga <ppalaga@redhat.com>
2014-05-09 16:44:47 +0200

    Using lama naming scheme in all contexts for arcGisGears

---
+++ src/main/webapp/js/arcGisGears.js
@@ -0,0 +1,9 @@
+var map;
+define("arcGisGears", [ "esri/map", "dojo/domReady!" ], function(Map) {
+    map = new Map("map", {
+        basemap : "topo",
+        center : [ -122.45, 37.75 ], // long, lat
+        zoom : 13,
+        sliderStyle : "small"
+    });
+});
